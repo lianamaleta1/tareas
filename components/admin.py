93377297b1741965115d46974fc1d721dd2244d1
Liana Maleta Terry <liana.maleta@etecsa.cu>
2024-02-24 11:36:23 -0500

    creating template view

--- components/admin.py
+++ components/admin.py
@@ -3,16 +3,15 @@ from django.db import models
 from django.forms.widgets import TextInput
 from .models import *
 
-class TareaAdmin(admin.ModelAdmin):
+class NoticiaAdmin(admin.ModelAdmin):
     # Personaliza el nombre del campo en el panel de administración
-    verbose_name = 'TareaPrincipal'
+    verbose_name = 'Noticias'
 
     # Personaliza la amplitud del campo en el panel de administración
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size': '400'})},
     } 
-admin.site.register(Tarea,TareaAdmin)
-admin.site.register(TareaIndividual)
-admin.site.register(TareaPrincipal)
-admin.site.register(Grupo)
-admin.site.register(TrabGrupo)
+admin.site.register(Noticia,NoticiaAdmin)
+admin.site.register(Documento)
+admin.site.register(CategoriaDocumento)
+admin.site.register(CategoriaIndicadores)
